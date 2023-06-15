7bcfcd970180ae026acba68041f0d8da2b5d8ccf
Uros Polanc <up4472@student.uni-lj.si>
2023-06-15 10:51:42 +0000

    v2023-06-15

--- source/script/cnn-zrimec.sh
+++ source/script/cnn-zrimec.sh
@@ -28,62 +28,6 @@ if [[ ":$PATH:" != *":$ROOT:"* ]]; then
 fi
 
 # Default params
-# --target_group   : global         : [str] tissue, group, age, global, perturbation
-# --target_type    : mean           : [str] mean, max
-# --target_explode : false          : [str] true, false
-# --target_filter  : none           : [str] none
-# --model_epochs   : 250            : [int] ...
-# --model_params   : none           : [int] none
-# --model_mode     : regression     : [str] regression, classification
-# --filter_id      : 0              : [int] ...
-# --generator      : group          : [str] stratified, group, random
-# --features       : true           : [str] true, false
-# --sequence_start : none           : [int] none
-# --sequence_end   : none           : [int] none
-
-# Run script
-python /d/hpc/home/up4472/workspace/upolanc-thesis/notebook/nbp08-cnn.py \
---target_group global \
---target_type mean \
---target_explode false \
---target_filter none \
---model_epochs 500 \
---model_params 0 \
---model_mode regression \
---filter_id 2 \
---generator group \
---features true \
---sequence_start none \
---sequence_end none#!/bin/bash
-
-#SBATCH --job-name=cnn-zrimec
-#SBATCH --output=/d/hpc/home/up4472/workspace/upolanc-thesis/slurm/zrimec-%j.out
-#SBATCH --error=/d/hpc/home/up4472/workspace/upolanc-thesis/slurm/zrimec-%j.err
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=64G
-#SBATCH --cpus-per-gpu=6
-#SBATCH --time=1-00:00:00
-
-# Activate conda enviorment
-source activate /d/hpc/home/up4472/anaconda3
-
-# Define root path
-ROOT="/d/hpc/home/up4472/workspace/upolanc-thesis/"
-
-# Ensure PYTHONPATH contains root
-if [[ ":$PYTHONPATH:" != *":$ROOT:"* ]]; then
-	export PYTHONPATH="$PYTHONPATH:$ROOT"
-fi
-
-# Ensure PATH contains root
-if [[ ":$PATH:" != *":$ROOT:"* ]]; then
-	export PATH="$PATH:$ROOT"
-fi
-
-# Default params
 # --target_group   : global          : [str] tissue, group, age, global, perturbation
 # --target_type    : mean            : [str] mean, max
 # --target_explode : false           : [str] true, false
